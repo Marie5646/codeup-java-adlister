54ff30805014151d50bfeb60023349e922fd1f16
Asia Autrey <asia.autrey0@gmail.com>
2023-05-16 14:18:33 -0500

    helloworldservlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,17 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "NameServlet",urlPatterns = "/hello")
+
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello World</h1>");
     }
 }
--- src/main/java/NameServlet.java
+++ src/main/java/NameServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.datatransfer.DataFlavor;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -14,6 +15,13 @@ public class NameServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
+
+        String name = req.getParameter("Asia");
         out.println("<h1>Hello, my name is Kotlin!</h1>");
+
+
+
     }
+
+
 }
