c7ba51306119a33699926b70766fe77050bd7801
Asia Autrey <asia.autrey0@gmail.com>
2023-05-21 12:22:15 -0500

    guess-exercise

---
+++ src/main/java/GuessServlet.java
@@ -0,0 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Random;
+
+@WebServlet(name = "GuessServlet", urlPatterns = "/guess")
+public class GuessServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/guess.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Random random = new Random();
+        int randomNum = random.nextInt(3) + 1;
+
+        System.out.println("The random number is:" + randomNum);
+        System.out.println("Your number is:" + Integer.parseInt(req.getParameter("userRand")));
+
+        if (randomNum == Integer.parseInt(req.getParameter("userRand"))){
+            resp.sendRedirect("/correct");
+        }else{
+            resp.sendRedirect("/incorrect");
+        }
+
+    }
+}
+
