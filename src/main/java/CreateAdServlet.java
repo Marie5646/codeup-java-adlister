b9dc96f1b4bd13783113ec7cd33eabf5e3c14404
Marie5646 <125408068+Marie5646@users.noreply.github.com>
2023-06-04 20:01:58 -0500

    user-input

--- src/main/java/CreateAdServlet.java
+++ src/main/java/CreateAdServlet.java
@@ -1,6 +1,6 @@
 import dao.DaoFactory;
 import models.Ad;
-
+import models.User;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,13 +11,19 @@ import java.io.IOException;
 @WebServlet(name = "CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            // add a return statement to exit out of the entire method.
+            return;
+        }
         request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
             .forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User loggedInUser = (User) request.getSession().getAttribute("user");
         Ad ad = new Ad(
-            1, // for now we'll hardcode the user id
+            loggedInUser.getId(),
             request.getParameter("title"),
             request.getParameter("description")
         );
